a6179761f5a70a4d9d85a21b37f866f95141c8d4
Christian Brel <ch.brel@gmail.com>
2015-04-05 03:50:56 +0200

    refs SERVICES-1 : Begin updated Tweet and TweetList.

--- scripts/infotype/Tweet.ts
+++ scripts/infotype/Tweet.ts
@@ -1,12 +1,87 @@
 /**
- * @author Simon Urli <simon@the6thscreen.fr>
+ * @author Christian Brel <christian@the6thscreen.fr, ch.brel@gmail.com>
  */
+
+/// <reference path="./Info.ts" />
 
 class Tweet extends Info {
 
-	private _username : string;
-	private _userrealname : string;
+	/**
+	 * Tweet's message.
+	 *
+	 * @property _message
+	 * @type string
+	 * @private
+	 */
 	private _message : string;
-	private _messageDate : Date;
 
+	/**
+	 * Constructor.
+	 *
+	 * @constructor
+	 */
+	constructor(id : string = "noId", priority : number = 0, creationDate : Date = null, obsoleteDate : Date = null, durationToDisplay : number = 10000, castingDate : Date = null,
+				message : string = null) {
+		super(id, priority, creationDate, obsoleteDate, durationToDisplay, castingDate);
+
+		this._message = message;
+	}
+
+	/**
+	 * Returns Tweet's message.
+	 *
+	 * @method getMessage
+	 * @returns {string} The Tweet's message.
+	 */
+	getMessage() : string {
+		return this._message;
+	}
+
+	/**
+	 * Set the Tweet's message.
+	 *
+	 * @method setMessage
+	 * @param {string} message - The new Tweet's message.
+	 */
+	setMessage(message : string) {
+		this._message = message;
+	}
+
+	/**
+	 * Return a Tweet instance from a JSON Object.
+	 *
+	 * @method fromJSONObject
+	 * @static
+	 * @param {JSONObject} json - The JSON Object
+	 * @return {Tweet} The InfoType instance.
+	 */
+	static fromJSONObject(jsonObject : any) : Tweet {
+		if (typeof(jsonObject._id) == "undefined") {
+			throw new InfoException("A Tweet object should have an ID.");
+		}
+		if(typeof(jsonObject._priority) == "undefined") {
+			throw new InfoException("A Tweet object should have a priority.");
+		}
+		if(typeof(jsonObject._creationDate) == "undefined") {
+			throw new InfoException("A Tweet object should have a creationDate.");
+		}
+		if(typeof(jsonObject._castingDate) == "undefined") {
+			throw new InfoException("A Tweet object should have a castingDate.");
+		}
+		if(typeof(jsonObject._obsoleteDate) == "undefined") {
+			throw new InfoException("A Tweet object should have an obsoleteDate.");
+		}
+		if(typeof(jsonObject._durationToDisplay) == "undefined") {
+			throw new InfoException("A Tweet object should have a durationToDisplay.");
+		}
+
+		if(typeof(jsonObject._message) == "undefined") {
+			throw new InfoException("A Tweet object should have a message.");
+		}
+
+		var t : Tweet = new Tweet(jsonObject._id, jsonObject._priority, jsonObject._creationDate, jsonObject._obsoleteDate, jsonObject._durationToDisplay, jsonObject._castingDate,
+			jsonObject._message);
+
+		return t;
+	}
 }
