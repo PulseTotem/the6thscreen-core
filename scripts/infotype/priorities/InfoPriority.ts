e34253f9a7fc26ddfb8e66909a1cebca74221ed8
Christian Brel <ch.brel@gmail.com>
2015-05-15 00:02:33 +0200

    refs CLIENT-23 : Added InfoPriority Enum for Info.

---
+++ scripts/infotype/priorities/InfoPriority.ts
@@ -0,0 +1,9 @@
+/**
+ * @author Christian Brel <christian@the6thscreen.fr, ch.brel@gmail.com>
+ * @author Simon Urli <simon@the6thscreen.fr, simon.urli@gmail.com>
+ */
+
+enum InfoPriority {
+	LOW,
+	HIGH
+}
