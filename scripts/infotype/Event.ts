1c62274fa536f022a1eef470b82db815d52e1055
Simon Urli <simon.urli@gmail.com>
2015-07-21 11:22:09 +0200

    CORECLIENT-58 Refactor CityEvent to adapt for Events

---
+++ scripts/infotype/Event.ts
@@ -0,0 +1,212 @@
+/**
+ * @author Simon Urli <simon@the6thscreen.fr>
+ */
+
+/// <reference path="./Info.ts" />
+/// <reference path="./exceptions/InfoException.ts" />
+
+/**
+ * Represent an event (e.g. from a Calendar)
+ *
+ * @class Event
+ */
+class Event extends Info {
+
+	/**
+	 * Define when the event starts
+	 *
+	 * @property _start
+	 * @type {Date}
+	 * @private
+	 */
+	private _start : Date;
+
+	/**
+	 * Define when the event ends
+	 *
+	 * @property _end
+	 * @type {Date}
+	 * @private
+	 */
+	private _end : Date;
+
+	/**
+	 * Define the name of the event
+	 *
+	 * @property _name
+	 * @type {string}
+	 * @private
+	 */
+	private _name : string;
+
+	/**
+	 * Define the description of the event
+	 *
+	 * @property _description
+	 * @type {string}
+	 * @private
+	 */
+	private _description : string;
+
+	/**
+	 * Define the location of the event
+	 *
+	 * @property _location
+	 * @type {string}
+	 * @private
+	 */
+	private _location : string;
+
+	/**
+	 * @method getStart
+	 * @returns {Date}
+	 */
+	public getStart() : Date {
+		return this._start;
+	}
+
+	/**
+	 * @method setStart
+	 * @param value
+	 */
+	public setStart(value:Date) {
+		this._start = value;
+	}
+
+	/**
+	 * @method getName
+	 * @returns {string}
+	 */
+	public getName():string {
+		return this._name;
+	}
+
+	/**
+	 * @method setName
+	 * @param value
+	 */
+	public setName(value:string) {
+		this._name = value;
+	}
+
+	/**
+	 * @method getEnd
+	 * @returns {Date}
+	 */
+	public getEnd():Date {
+		return this._end;
+	}
+
+	/**
+	 * @method setEnd
+	 * @param value
+	 */
+	public setEnd(value:Date) {
+		this._end = value;
+	}
+
+	/**
+	 * @method getDescription
+	 * @returns {string}
+	 */
+	public getDescription() : string {
+		return this._description;
+	}
+
+	/**
+	 * @method setDescription
+	 * @param description
+	 */
+	public setDescription(description : string) {
+		this._description = description;
+	}
+
+	/**
+	 * @method getLocation
+	 * @returns {string}
+	 */
+	public getLocation() : string {
+		return this._location;
+	}
+
+	/**
+	 * @method setLocation
+	 * @param location
+	 */
+	public setLocation(location : string) {
+		this._location = location;
+	}
+
+	/**
+	 * Constructor.
+	 *
+	 * @constructor
+	 */
+	constructor(id : string = "noId", priority : number = 0, creationDate : Date = null, obsoleteDate : Date = null, durationToDisplay : number = 10, castingDate : Date = null,
+	            start : Date = null, end : Date = null, name : string = null, description : string = null, location : string = null) {
+		super(id, priority, creationDate, obsoleteDate, durationToDisplay, castingDate);
+
+		this.setStart(start);
+		this.setEnd(end);
+		this.setName(name);
+		this.setDescription(description);
+		this.setLocation(location);
+	}
+
+	static fromJSONObject(jsonObject : any) : Event {
+		if (typeof(jsonObject._id) == "undefined") {
+			throw new InfoException("An Event object should have an ID.");
+		}
+		if(typeof(jsonObject._priority) == "undefined") {
+			throw new InfoException("An Event object should have a priority.");
+		}
+		if(typeof(jsonObject._creationDate) == "undefined") {
+			throw new InfoException("An Event object should have a creationDate.");
+		}
+		if(typeof(jsonObject._castingDate) == "undefined") {
+			throw new InfoException("An Event object should have a castingDate.");
+		}
+		if(typeof(jsonObject._obsoleteDate) == "undefined") {
+			throw new InfoException("An Event object should have an obsoleteDate.");
+		}
+		if(typeof(jsonObject._durationToDisplay) == "undefined") {
+			throw new InfoException("An Event object should have a durationToDisplay.");
+		}
+
+		if(typeof(jsonObject._name) == "undefined") {
+			throw new InfoException("An Event object should have a name.");
+		}
+		if(typeof(jsonObject._start) == "undefined") {
+			throw new InfoException("An Event object should have a start.");
+		}
+		if(typeof(jsonObject._end) == "undefined") {
+			throw new InfoException("An Event object should have an end.");
+		}
+		if(typeof(jsonObject._description) == "undefined") {
+			throw new InfoException("An Event object should have a description.");
+		}
+		if(typeof(jsonObject._location) == "undefined") {
+			throw new InfoException("An Event object should have a location.");
+		}
+
+		var e : Event = new Event(jsonObject._id, jsonObject._priority, jsonObject._creationDate, jsonObject._obsoleteDate, jsonObject._durationToDisplay, jsonObject._castingDate);
+		e.setName(jsonObject._name);
+		e.setStart(jsonObject._start);
+		e.setEnd(jsonObject._end);
+		e.setDescription(jsonObject._description);
+		e.setLocation(jsonObject._location);
+
+		return e;
+	}
+
+	/**
+	 * Check if 'this' is equal to info in param.
+	 *
+	 * @method equals
+	 * @param {Info} info - Info to update.
+	 * @return {boolean} 'true' if objects are equals, 'false' otherwise
+	 */
+	equals(info : Event) : boolean {
+		return (this.getName() == info.getName() && this.getStart() == info.getStart() && this.getEnd() == info.getEnd() && this.getDescription() == info.getDescription() && this.getLocation() == info.getLocation());
+	}
+}
--- scripts/infotype/EventList.ts
+++ scripts/infotype/EventList.ts
@@ -3,18 +3,88 @@
  */
 
 /// <reference path="./Info.ts" />
-/// <reference path="./CityEvent.ts" />
+/// <reference path="./Event.ts" />
 /// <reference path="./exceptions/InfoException.ts" />
 
+/**
+ * Represent a list of events
+ *
+ * @class EventList
+ */
 class EventList extends Info {
 
-	private _events : Array<CityEvent>;
+	/**
+	 * Define the name of the EventList
+	 *
+	 * @property _name
+	 * @type {string}
+	 * @private
+	 */
+	private _name : string;
 
-	public getEvents() : Array<CityEvent> {
+	/**
+	 * Define the description of the EventList
+	 *
+	 * @property _description
+	 * @type {string}
+	 * @private
+	 */
+	private _description : string;
+
+	/**
+	 * Define the list of events
+	 *
+	 * @property _events
+	 * @type {Array<Event>}
+	 * @private
+	 */
+	private _events : Array<Event>;
+
+	/**
+	 * @method getName
+	 * @returns {string}
+	 */
+	public getName() : string {
+		return this._name;
+	}
+
+	/**
+	 * @method setName
+	 * @param name
+	 */
+	public setName(name : string) {
+		this._name = name;
+	}
+
+	/**
+	 * @method getDescription
+	 * @returns {string}
+	 */
+	public getDescription() : string {
+		return this._description;
+	}
+
+	/**
+	 * @method setDescription
+	 * @param description
+	 */
+	public setDescription(description : string) {
+		this._description = description;
+	}
+
+	/**
+	 * @method getEvents
+	 * @returns {Array<Event>}
+	 */
+	public getEvents() : Array<Event> {
 		return this._events;
 	}
 
-	public addEvent(e : CityEvent) {
+	/**
+	 * @method addEvent
+	 * @param e
+	 */
+	public addEvent(e : Event) {
 		this._events.push(e);
 	}
 
@@ -24,8 +94,11 @@ class EventList extends Info {
 	 * @constructor
 	 */
 	constructor(id : string = "noId", priority : number = 0, creationDate : Date = null, obsoleteDate : Date = null, durationToDisplay : number = 10, castingDate : Date = null,
-	            events : Array<CityEvent> = new Array<CityEvent>()) {
+	            name : string = "", description : string = "", events : Array<Event> = new Array<Event>()) {
 		super(id, priority, creationDate, obsoleteDate, durationToDisplay, castingDate);
+
+		this.setName(name);
+		this.setDescription(description);
 		this._events = events;
 	}
 
@@ -49,15 +122,24 @@ class EventList extends Info {
 			throw new InfoException("A EventList object should have a durationToDisplay.");
 		}
 
+		if(typeof(jsonObject._name) == "undefined") {
+			throw new InfoException("A EventList object should have a name.");
+		}
+		if(typeof(jsonObject._description) == "undefined") {
+			throw new InfoException("A EventList object should have a description.");
+		}
 		if(typeof(jsonObject._events) == "undefined") {
 			throw new InfoException("A EventList object should have events.");
 		}
 
 		var el : EventList = new EventList(jsonObject._id, jsonObject._priority, jsonObject._creationDate, jsonObject._obsoleteDate, jsonObject._durationToDisplay, jsonObject._castingDate);
 
+		el.setName(jsonObject._name);
+		el.setDescription(jsonObject._description);
+
 		for(var i = 0; i < jsonObject._events.length; i++) {
 			var pDesc = jsonObject._events[i];
-			var p : CityEvent = CityEvent.fromJSONObject(pDesc);
+			var p : Event = Event.fromJSONObject(pDesc);
 			el.addEvent(p);
 		}
 
@@ -72,15 +154,16 @@ class EventList extends Info {
 	 * @return {boolean} 'true' if objects are equals, 'false' otherwise
 	 */
 	equals(info : EventList) : boolean {
-		if(this.getEvents().length != info.getEvents().length) {
+
+		if (this.getName() != info.getName() || this.getDescription() != info.getDescription() || this.getEvents().length != info.getEvents().length) {
 			return false;
 		} else {
 			var equalStatus = true;
 
-			this.getEvents().forEach(function (event : CityEvent) {
+			this.getEvents().forEach(function (event : Event) {
 				var existEqual = false;
 
-				info.getEvents().forEach(function(otherEvent : CityEvent) {
+				info.getEvents().forEach(function(otherEvent : Event) {
 					if(!existEqual) {
 						existEqual = event.equals(otherEvent);
 					}
